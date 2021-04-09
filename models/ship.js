61d23ab9334fb837d3a82e0f8a5f814191d02ec5
dharmawinata <dharma.winata92@gmail.com>
2021-04-09 07:54:12 +0700

    update routes models controllers

--- models/ship.js
+++ models/ship.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      // Ship.hasMany(models.Pirate)
+      Ship.hasMany(models.Pirate)
     }
   };
   Ship.init({
--- routes/pirate.js
+++ routes/pirate.js
@@ -2,4 +2,11 @@ const PirateRouter = require('express').Router();
 
 const PirateController = require('../controllers/PirateController')
 
+PirateRouter.get('/', (PirateController.readPirates));
+
+PirateRouter.get('/add', PirateController.addPagePirate);
+PirateRouter.post('/add', PirateController.addPirate);
+
+PirateRouter.get('/delete/:id', PirateController.deletePirate)
+
 module.exports = PirateRouter;
